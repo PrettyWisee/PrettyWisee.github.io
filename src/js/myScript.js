46b8e9950ff42275f99b5fb1e1f2a70268f9554e
PrettyWisee <94062012+PrettyWisee@users.noreply.github.com>
2021-12-11 18:35:49 +0300

    1

---
+++ src/js/myScript.js
@@ -0,0 +1,45 @@
+"use strict"
+
+var siteType = {a:"10",b:"20",c:"30"};
+
+
+var siteType = ("a");
+
+if (siteType == "a") {
+  console.log(10);
+}
+
+else {
+console.log(0);
+}
+
+if (siteType == "b") {
+  console.log(20);
+}
+else {
+  console.log(0);
+}
+
+if (siteType == "c") {
+ console.log(0);
+}
+
+let Design = {
+  a:  5,
+  b: 10,
+  c: 15,
+};
+console.log(Design);
+let Adaptability = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+console.log(Adaptability);
+
+let siteTypetwo = prompt ("siteType","a,b,c")
+
+console.log(siteTypetwo)
+
+prompt("Design","a,b,c");
+prompt("Adaptability","a,b,c");
