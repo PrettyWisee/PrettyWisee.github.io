cd859803cc71cf819a05070ef163ba2c627a8fe2
PrettyWisee <94062012+PrettyWisee@users.noreply.github.com>
2021-12-15 23:53:39 +0300

    Add files via upload

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,53 +1,76 @@
 "use strict"
+$(document).ready(function() {
+  $(window).scroll(() => {
+    let scrollDistance = $(window).scrollTop();
+    $(".section").each((i, el) => {
+      if ($(el).offset().top - $("nav").outerHeight() <= scrollDistance) {
+        $("nav a").each((i, el) => {
+          if ($(el).hasClass("menu li a")) {
+            $(el).removeClass("menu li a");
+          }
+        });
+        $('nav li:eq('+ i +')').find('a').addClass('menu li a');
+      }
+    });
+  });
+});
 
-var siteType = {b:"a",a:"b",c:"c"};
+var siteType = {
+  b: "a",
+  a: "b",
+  c: "c"
+};
 
 let Design = {
-  a:  "a",
+  a: "a",
   b: "b",
   c: "c",
 };
 console.log(Design);
 let Adaptability = {
-  a: 1,
-  b: 2,
-  c: 3,
+  a: "a",
+  b: "b",
+  c: "c",
 };
 console.log(Adaptability);
 let sum = 0;
 
 
-let siteTypetwo = prompt ("siteType","a,b,c")
-if (siteType == "a"){
+let siteTypetwo = prompt("siteType", "a,b,c")
+if (siteTypetwo == "a") {
   sum = sum + 100;
-}
-else if (siteType == "b") {
+} else if (siteTypetwo == "b") {
   sum = sum + 50;
-} else if (siteType == "c"){
-sum == sum + 10;
+} else if (siteTypetwo == "c") {
+  sum = sum + 10;
 };
-console.log(siteTypetwo);
 
 
-let DesignTwo = prompt("Design","a,b,c")
-if (Design == "a"){
+
+let DesignTwo = prompt("Design", "a,b,c")
+if (DesignTwo == "a") {
   sum = sum + 300;
-}
-else if (Design == "b") {
+} else if (DesignTwo == "b") {
   sum = sum + 540;
-} else if (Design == "c"){
-sum == sum + 170;
+} else if (DesignTwo == "c") {
+  sum = sum + 170;
 };
-console.log(Design);
 
-let AdaptabilityTwo = prompt("Adaptability","1,2,3")
-if (Adaptability == "1"){
+
+let AdaptabilityTwo = prompt("Adaptability", "1,2,3")
+if (AdaptabilityTwo == "a") {
   sum = sum + 600;
-}
-else if (Adaptability == "2") {
+} else if (AdaptabilityTwo == "b") {
   sum = sum + 940;
-} else if (Adaptability == "3"){
-sum == sum + 270;
+} else if (AdaptabilityTwo == "c") {
+  sum = sum + 270;
 };
 
 console.log(sum);
+alert("Сумма =" + sum);
+$('a[href^="#"]').click(function() {
+  let valHref = $(this).attr("href");
+  $('html, body').animate({
+    scrollTop: $(valHref).offset().top - 60 + "px"
+  });
+});
